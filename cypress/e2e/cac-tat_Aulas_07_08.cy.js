e8c3dde8fbdee06d979442874463f56d844229d9
unknown <you@example.com>
2024-01-29 17:59:50 -0300

    Exercicios da aula 07. Lidando com link que abre na outra aba.

--- cypress/e2e/cac-tat_Aulas_07_08.cy.js
+++ cypress/e2e/cac-tat_Aulas_07_08.cy.js
@@ -6,15 +6,21 @@ describe('Aula 07 - Novas abas abertas', () => {
     })
 
     it('Verificando que uma nova aba foi aberta', () => {
-
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
     })
 
     it('Removendo o atributo "target" para o link ser aberto na mesma aba', () => {
-
+        cy.get('#privacy a').invoke('removeAttr', 'target').click()
+        
+        cy.title('Central de Atendimento ao Cliente TAT - Política de privacidade')
+        .should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
     })
 
     it('Substituindo o atributo "target" para o link ser aberto na mesma aba', () => {
-        
+        cy.get('#privacy a').invoke('attr', 'target', '_self').click()
+
+        cy.title('Central de Atendimento ao Cliente TAT - Política de privacidade')
+        .should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
     })
 
 })
